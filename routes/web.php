7021179d2fe33e05b324d073d65f80f910f2e986
Michel Souza <91084191+MichelNsouza@users.noreply.github.com>
2023-09-16 18:51:40 -0300

    trocando rotas

--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,7 @@
 use App\Http\Controllers\NewsController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/noticias', [NewsController::class, "index"])->name("news.index");
+Route::get('/', [NewsController::class, "index"])->name("news.index");
 
 Route::get('/noticias/novas', [NewsController::class, "create"])->name("news.create");
 
