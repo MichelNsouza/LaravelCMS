aff2c01bdcbde5e8510a06add5ab7b06d491db8a
Michel Souza <91084191+MichelNsouza@users.noreply.github.com>
2024-03-08 15:55:30 -0300

    Update database.php

--- config/database.php
+++ config/database.php
@@ -65,17 +65,15 @@ return [
 
         'pgsql' => [
             'driver' => 'pgsql',
-            'url' => env('DATABASE_URL'),
-            'host' => env('DB_HOST', '127.0.0.1'),
-            'port' => env('DB_PORT', '5432'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            'host' => 'ep-yellow-block-23083958.us-east-1.aws.neon.tech',
+            'port' => 5432,
+            'database' => 'verceldb',
+            'username' => 'default',
+            'password' => 'TQourpNZ64fS',
             'charset' => 'utf8',
             'prefix' => '',
             'prefix_indexes' => true,
-            'search_path' => 'public',
-            'sslmode' => 'prefer',
+            'sslmode' => 'require', // Defina o modo SSL como 'require'
         ],
 
         'sqlsrv' => [
